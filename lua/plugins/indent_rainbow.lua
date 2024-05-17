dd4ebeb5c60162e9034bf43f317f49fa9c18b4fe
Henrique <henrique@xonha.space>
2024-05-17 14:16:00 -0300

    fix: indent-blankline version 3

--- lua/plugins/indent_rainbow.lua
+++ lua/plugins/indent_rainbow.lua
@@ -1,5 +1,7 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  commit = "29be0919b91fb59eca9e90690d76014233392bef",
   dependencies = {
     "TheGLander/indent-rainbowline.nvim",
   },
